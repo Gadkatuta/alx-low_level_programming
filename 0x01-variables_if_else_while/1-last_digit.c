8e984a30382b2bed9b5048ffc27f86af649b2e97
gadkatuta <gadkatuta@gmail.com>
2023-02-19 04:03:32 -0800

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s %d is %d and is ", last, n, n %10);
+	printf(" %s %d is %d and is", last, n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
@@ -25,10 +25,9 @@ int main(void)
 	{
 		printf("0\n");
 	}
-	else 
+	else
 	{
 		printf("less than 6 and not 0\n");
 	}
-	
-  return (0);
+	return (0);
 }
