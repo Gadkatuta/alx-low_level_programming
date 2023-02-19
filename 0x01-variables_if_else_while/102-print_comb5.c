7e98a49b40f127c9c94254708e01421a9d76f495
gadkatuta <gadkatuta@gmail.com>
2023-02-19 11:39:50 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,11 +4,12 @@
  *   * two digit numbers, without any repetition.
  *   (*
  *    * Return: 0 on success.
- *     */
+ */
 
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
