ac900317526971fc39e703b05d969771a8394081
gadkatuta <gadkatuta@gmail.com>
2023-02-19 11:28:37 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,10 +3,11 @@
  *  * main - prints all possible combinations of single digit numbers.
  *  (*
  *   * Return: 0 on success
- *    */
+ */
 int main(void)
 {
 	int i = '0';
+
 	while (i <= '9')
 	{
 		putchar(i);
@@ -20,4 +21,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-	
