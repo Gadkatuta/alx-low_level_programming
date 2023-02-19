2d1b3998ef33a5f8c4829366a776a7c09d522eff
gadkatuta <gadkatuta@gmail.com>
2023-02-19 11:17:39 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Prints the numbers from 0-9.
- *  (*
- *   * Return: 0 on success
- *    */
+ * main - Prints the numbers from 0-9.
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("0123456789\n");
